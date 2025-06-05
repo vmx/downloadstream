9fd980729f09f8931cf7eaa431a627d018f07667
Volker Mische <volker.mische@gmail.com>
2025-06-06 00:01:42 +0200

    fix: remove logging

--- sw.js
+++ sw.js
@@ -12,7 +12,6 @@ const createReadableStream = (port) => {
   return new ReadableStream({
     start(controller) {
       port.onmessage = (event) => {
-        console.log('vmx: sw: within readable stream: received data:', event.data)
         if (event.data == 'close') {
           controller.close()
           return
@@ -31,7 +30,6 @@ const createReadableStream = (port) => {
       }
     },
     cancel(reason) {
-      console.log('vmx: sw: within readable stream: stop it3')
       // When the stream is closed, send a message to the originator, so that
       // things can be cleaned up. Also close the port so that we don't
       // continue to accept data, even if it's sent.
@@ -43,14 +41,12 @@ const createReadableStream = (port) => {
 
 
 self.addEventListener('message', (event) => {
-  console.log('vmx: message from sw: event:', event)
   // When a port is supplied, it means that a new download should be started.
   // All other messages are ignored, they are only sent to keep the service
   // worker alive
   if (event.ports.length == 1) {
     const port = event.ports[0]
     const pathname = event.data.pathname
-    console.log('vmx: message from sw: pathname, port:', pathname, port)
     const stream = createReadableStream(port)
     streams.set(pathname, stream)
   }
@@ -60,7 +56,6 @@ self.addEventListener('message', (event) => {
 // is set, so that there's a download file dialog.
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url)
-  console.log('vmx: sw: intercepting fetch: event:', event)
 
   const stream = streams.get(url.pathname)
 
@@ -70,7 +65,6 @@ self.addEventListener('fetch', (event) => {
     return
   }
 
-  console.log('vmx: sw: intercepting fetch: stream:', stream)
 
   const headers = new Headers({
     'Content-Type': 'application/octet-stream',
